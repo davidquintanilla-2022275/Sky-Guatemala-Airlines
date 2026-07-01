2bc3d3aafd59260fc6be2076ee4af8f3cebfc74f
David <dquintanilla-2022275@kinal.edu.gt>
2026-07-01 11:05:52 -0600

    Feat: Se agrego el mapeo de la entidad de Vuelo

---
+++ Sky-Guatemala/src/models/Vuelo.ts
@@ -0,0 +1,11 @@
+import {Ruta} from "./Ruta";
+
+export interface Vuelo {
+    id_vuelo: number;
+    numero_vuelo: string;
+    origen: string;
+    destino: string;
+    fecha_salida: string;
+    fecha_llegada: string;
+    id_ruta: Ruta;
+}
