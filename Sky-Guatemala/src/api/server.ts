2f8ccfad6c2a90c9a911187c27841350e0ec82f4
David Enrique Quintanilla Pérez <dquintanilla-2022275@kinal.edu.gt>
2026-07-26 20:00:47 -0600

    Fix: se reformo el server

--- Sky-Guatemala/src/api/server.ts
+++ Sky-Guatemala/src/api/server.ts
@@ -1,18 +1,46 @@
-import { createServer } from 'http';
-import { routes } from '../routes/Clienteroutes';
- 
+import { createServer } from "http";
+import { routes as clienteRoutes } from "../routes/Clienteroutes";
+import { routes as empleadoRoutes } from "../routes/Empleadorotes";
+import { routes as envioRoutes } from "../routes/Envioroutes";
+import { routes as pagoRoutes } from "../routes/Pagoroutes";
+import { routes as paqueteRoutes } from "../routes/Paqueteroutes";
+
 const servidor = createServer(async (req, res) => {
- 
-    await routes(req, res);
- 
+
+    const url = req.url ?? "";
+
+    if (url.startsWith("/Clientes")) {
+        return clienteRoutes(req, res);
+    }
+
+    if (url.startsWith("/Empleados")) {
+        return empleadoRoutes(req, res);
+    }
+
+    if (url.startsWith("/Envio")){
+        return envioRoutes(req, res);
+    }
+
+    if (url.startsWith("/Pago")){
+        return pagoRoutes(req, res);
+    }
+
+    if (url.startsWith("/Paquete")){
+        return paqueteRoutes(req,res);
+    }
+
+    res.writeHead(404, {
+        "Content-Type": "application/json"
+    });
+
+    res.end(JSON.stringify({
+        mensaje: "Ruta no encontrada"
+    }));
 });
- 
+
 servidor.listen(3000, () => {
-     console.log('===================================');
- 
-     console.log('Servidor iniciado');
- 
-     console.log('http://localhost:3000');
-     
-     console.log('===================================');
+    console.log("===================================");
+    console.log("Servidor iniciado");
+    console.log("http://localhost:3000");
+    console.log("===================================");
 });
