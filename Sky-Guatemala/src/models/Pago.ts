14bfdac54b430e639661a63f999a538837893b73
David Enrique Quintanilla Pérez <dquintanilla-2022275@kinal.edu.gt>
2026-07-27 15:41:20 -0600

    Fix: se modifico el modelo de la entidad Pago

--- Sky-Guatemala/src/models/Pago.ts
+++ Sky-Guatemala/src/models/Pago.ts
@@ -1,9 +1,7 @@
-import { Envio } from "./Envio";
-
-export interface Pago{
+export interface Pago {
     id_pago: number;
     monto: number;
     metodo_pago: string;
     fecha_pago: string;
-    id_envio: Envio;
+    id_envio: number;
 }
