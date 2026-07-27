2b7948d2b4d8f90f536dfe53c483805c64fa87a5
David Enrique Quintanilla Pérez <dquintanilla-2022275@kinal.edu.gt>
2026-07-27 17:34:03 -0600

    Feat: se modifico el model de Tracking

--- Sky-Guatemala/src/models/Tracking.ts
+++ Sky-Guatemala/src/models/Tracking.ts
@@ -1,11 +1,8 @@
-import { Envio } from "./Envio";   
-
 export interface Tracking {
     id_tracking: number;
     codigo_tracking: string;
     ubicacion_actual: string;
     estado_actual: string;
-    fecha_actualizacion: Date;
-    id_envio: Envio;
-
+    fecha_actualizacion: string;
+    id_envio: number;
 }
