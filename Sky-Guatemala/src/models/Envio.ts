08b71b8fe3a93638beda06182fccfcbb5b06442b
David Enrique Quintanilla Pérez <dquintanilla-2022275@kinal.edu.gt>
2026-07-27 15:31:06 -0600

    Fix: se modifico el modelo de la entidad de Envio

--- Sky-Guatemala/src/models/Envio.ts
+++ Sky-Guatemala/src/models/Envio.ts
@@ -1,12 +1,8 @@
-import { Sucursal } from "./Sucursal";
-import { Cliente } from "./Cliente";
-
-
 export interface Envio {
     id_envio: number;
     fecha_envio: string;
     estado: string;
     costo: number;
-    sucursal: Sucursal;
-    cliente: Cliente;
+    sucursal: number;
+    cliente: number;
 }
