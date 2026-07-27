a29b207c7b45af243f451eb82bf8fd7dd516899e
David Enrique Quintanilla Pérez <dquintanilla-2022275@kinal.edu.gt>
2026-07-27 15:28:17 -0600

    Fix: se modifico el modelo de la entidad de empleado

--- Sky-Guatemala/src/models/Empleado.ts
+++ Sky-Guatemala/src/models/Empleado.ts
@@ -1,10 +1,8 @@
-import { Sucursal } from "./Sucursal";
-
 export interface Empleado {
     id_empleado: number;
     nombre: string;
     puesto: string;
     telefono: string;
-    id_sucursal: Sucursal;
+    id_sucursal: number;
 
 }
