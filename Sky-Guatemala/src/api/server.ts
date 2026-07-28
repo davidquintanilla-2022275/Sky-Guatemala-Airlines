ea8110cd98308a8bf46cd8edb39a0658a2eac3ec
David Enrique Quintanilla Pérez <dquintanilla-2022275@kinal.edu.gt>
2026-07-27 18:37:56 -0600

    Feat: se termino de modificar el server del proyecto

--- Sky-Guatemala/src/api/server.ts
+++ Sky-Guatemala/src/api/server.ts
@@ -4,6 +4,11 @@ import { routes as empleadoRoutes } from "../routes/Empleadorotes";
 import { routes as envioRoutes } from "../routes/Envioroutes";
 import { routes as pagoRoutes } from "../routes/Pagoroutes";
 import { routes as paqueteRoutes } from "../routes/Paqueteroutes";
+import { routes as rutaRoutes } from "../routes/Rutaroutes";
+import { routes as sucursalRoutes } from "../routes/Sucursalroutes";
+import { routes as trackingRoutes } from "../routes/Trackingroutes";
+import { routes as vehiculoRoutes } from "../routes/Vehiculoroutes";
+import { routes as vueloRoutes } from "../routes/Vueloroutes";
 
 const servidor = createServer(async (req, res) => {
 
@@ -27,6 +32,26 @@ const servidor = createServer(async (req, res) => {
 
     if (url.startsWith("/Paquete")){
         return paqueteRoutes(req,res);
+    }
+
+    if (url.startsWith("/Ruta")){
+        return rutaRoutes(req, res);
+    }
+
+    if (url.startsWith("/Sucursal")){
+        return sucursalRoutes(req, res);
+    }
+
+    if (url.startsWith("/Tracking")){
+        return trackingRoutes(req, res);
+    }
+
+    if (url.startsWith("/Vehiculo")){
+        return vehiculoRoutes(req, res);
+    }
+
+    if (url.startsWith("/Vuelo")){
+        return vueloRoutes(req, res);
     }
 
     res.writeHead(404, {
